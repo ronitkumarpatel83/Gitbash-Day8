beab228d0e3adcbbd0a5f3daafa8c6ff36fdfeb4
unknown <ronitkumarpatel83@gmail.com>
2022-03-11 16:04:39 +0530

    added file

---
+++ dictionary.sh
@@ -0,0 +1,14 @@
+declare -A holidays
+holiday[jan]="Republic day"
+holiday[feb]="Shivaratri"
+holiday[march]="holi"
+holiday[april]="good friday"
+holiday[may]="May day"
+holiday[june]="none"
+holiday[july]="eid"
+holiday[aug]="independence day"
+holiday[sep]="ganesh puja"
+holiday[oct]="dashera"
+holiday[nov]="diwali"
+holiday[dec]="x-mass"
+
